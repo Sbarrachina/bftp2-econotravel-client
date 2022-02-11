9196ef7f54d5c23e7bb6dfd461d8fde04220c2a9
Sbarrachina <sandrabarrachina1978@gmail.com>
2022-02-11 12:19:40 +0100

    carrousel creado imagenes y textos

--- src/components/pages/Catalogo.js
+++ src/components/pages/Catalogo.js
@@ -1,6 +1,10 @@
 import React from 'react';
 import {useEffect, useState} from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
+
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';
+
 const Catalogo = () => {
 
     const [experiences, setExperiences] = useState([]);
@@ -32,11 +36,30 @@ const Catalogo = () => {
                 <div className="catalog-gallery">
                     <p>Las mejores actividades que hacer</p>
                     <div className="experience-list">
-                        {experiences.map(e => <div className="experience">
-                            <img className="experience-image"  src={e.image} alt="experience - image"/>
+                        <Carousel>
+                            <div>
+                                <img src="assets/bici.png" />
+                                <p className="legend">Paseo en bicicleta por el Montseny</p>
+                            </div>
+                            <div>
+                                <img src="assets/barco.png" />
+                                <p className="legend">Descubre las costa en barco de vela</p>
+                            </div>
+                            <div>
+                                <img src="assets/bcnmodernista.png" />
+                                <p className="legend">Descubre la Barcelona Modernista de noche</p>
+                            </div>
 
-                            <p>{e.name}</p>
-                        </div> )}
+                            <div>
+                                <img src="assets/huerta.png" />
+                                <p className="legend">Del huerto a la mesa</p>
+                            </div>
+                            <div>
+                                <img src="assets/montserrat.png" />
+                                <p className="legend">Arte en la montaña sagrada</p>
+                            </div>
+                        </Carousel>
+
                     </div>
                 </div>
            </div>
